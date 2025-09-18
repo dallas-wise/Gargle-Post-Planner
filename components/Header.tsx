b7fd67d8050b5954bacfe05d8f384ad9623b0ce6
Dallas Wise <dtwise19@gmail.com>
2025-09-18 15:28:16 -0600

    Header edits

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,9 @@ import GargleLogo from '../assets/Gargle-Logo.png';
 
 export const Header: React.FC = () => {
   return (
-    <header className="bg-white shadow-sm border-b border-slate-200">
+    <header className="bg-gray-50 shadow-sm border-b border-slate-200">
       <div className="container mx-auto px-4 py-6 flex justify-center">
-        <img src={GargleLogo} alt="Gargle" className="h-12 w-auto" />
+        <img src={GargleLogo} alt="Gargle" className="h-16 w-auto" />
       </div>
     </header>
   );
