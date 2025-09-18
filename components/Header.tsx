d85c486734e43a701900b11f703979d5def614d9
Dallas Wise <dtwise19@gmail.com>
2025-09-18 15:20:38 -0600

    logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import GargleLogo from '../assets/Gargle-Logo.png';
 
 export const Header: React.FC = () => {
   return (
     <header className="bg-white shadow-sm border-b border-slate-200">
       <div className="container mx-auto px-4 py-6 flex justify-center">
-        <img src="/Gargle-Logo.png" alt="Gargle" className="h-12 w-auto" />
+        <img src={GargleLogo} alt="Gargle" className="h-12 w-auto" />
       </div>
     </header>
   );
